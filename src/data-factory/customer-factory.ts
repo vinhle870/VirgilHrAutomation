93a9916647876524bf9dca7c752ba83289fe97c3
Vinh Le <VinhleAvantage@gmail.com>
2026-02-08 22:09:57 +0700

    Update customer factory

--- src/data-factory/customer-factory.ts
+++ src/data-factory/customer-factory.ts
@@ -9,91 +9,41 @@ export class CustomerFactory {
     overrides?: Partial<Record<string, any>>,
     plan?: string
   ): Promise<MembPortalCustomer> {
+
     const customer = new MembPortalCustomer();
 
-    const ts = format(new Date(), "yyyyMMddHHmmss");
+    const ts = format(new Date(), 'yyyyMMddHHmmss');
     const seq = DataGenerate.getRandomInt(1, 9999);
-    const firstName =
-      overrides?.firstName ?? (await DataGenerate.generateFirstName());
+    const firstName = overrides?.firstName ?? await DataGenerate.generateFirstName();
     const localPrefix = overrides?.firstName ?? `${firstName}${seq}`;
     const email = overrides?.email ?? `${localPrefix}@yopmail.com`;
     const password = overrides?.password ?? `Vl@${ts.slice(-8)}`;
-    const lastName =
-      overrides?.lastName ?? (await DataGenerate.generateLastName());
-    const jobTitle =
-      overrides?.jobTitle ?? (await DataGenerate.generatejobTitle());
-    const companyName =
-      overrides?.companyName ?? (await DataGenerate.generateCompanyName());
-    const companySize = overrides?.companySize ?? "";
-    const phoneNumber =
-      overrides?.phoneNumber ?? (await DataGenerate.generatePhoneNumber());
-    const partnerId = overrides?.partnerId;
-    const source = overrides?.source ?? "member";
-    const departmentId = overrides?.departmentId ?? "688897d5eb52b4af5573def4";
+    const lastName = overrides?.lastName ?? await DataGenerate.generateLastName();
+    const jobTitle = overrides?.jobTitle ?? await DataGenerate.generatejobTitle();
+    const companyName = overrides?.companyName ?? await DataGenerate.generateCompanyName();
+    const companySize = overrides?.companySize ?? '';
+    const phoneNumber = overrides?.phoneNumber ?? await DataGenerate.generatePhoneNumber();
+    const partnerId = overrides?.partnerId ?? null;
+    const source = overrides?.source ?? 'member';
+    const departmentId = overrides?.departmentId ?? '688897d5eb52b4af5573def4';
     const ssoProvider = overrides?.ssoProvider ?? null;
     const ssoToken = overrides?.ssoToken ?? null;
 
-<<<<<<< .mine
     //Admin Portal specific info
-    const userType = overrides?.userType ?? "0"; // Default to '0' - Admin;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
-        const customer = new MembPortalCustomer();
-
-        const ts = format(new Date(), 'yyyyMMddHHmmss');
-        const seq = DataGenerate.getRandomInt(1, 9999);
-        const firstName = overrides?.firstName ?? await DataGenerate.generateFirstName();
-        const localPrefix = overrides?.firstName ?? `${firstName}${seq}`;
-        const email = overrides?.email ?? `${localPrefix}@yopmail.com`;
-        const password = overrides?.password ?? `Vl@${ts.slice(-8)}`;
-        const lastName = overrides?.lastName ?? await DataGenerate.generateLastName();
-        const jobTitle = overrides?.jobTitle ?? await DataGenerate.generatejobTitle();
-        const companyName = overrides?.companyName ?? await DataGenerate.generateCompanyName();
-        const companySize = overrides?.companySize ?? '';
-        const phoneNumber = overrides?.phoneNumber ?? await DataGenerate.generatePhoneNumber();
-        const partnerId = overrides?.partnerId?? null;
-        const source = overrides?.source ?? 'member';
-        const departmentId = overrides?.departmentId ?? '688897d5eb52b4af5573def4';
-        const ssoProvider = overrides?.ssoProvider ?? null;
-        const ssoToken = overrides?.ssoToken ?? null;
-
-        //Admin Portal specific info
     const userType = overrides?.userType ?? '0' // Default to '0' - Admin;
->>>>>>> .theirs
     const useCredit = overrides?.useCredit ?? false;
-
     const statesEmployee = overrides?.statesEmployee ?? [];
     const country = overrides?.country ?? validCountry;
     const totalEmployees = overrides?.totalEmployees ?? 0;
-
     const isSso = overrides?.isSso ?? false;
     const type = overrides?.type ?? 1;
-
     const partnerConsultantId = overrides?.partnerConsultantId ?? "";
     const industry = overrides?.industry ?? [validIndustry];
     const productType = overrides?.productType ?? '';
     const billingcycle = overrides?.billingcycle;
     const trialDays = overrides?.trialDays;
 
-        if (plan) {
+    if (plan) {
     }
 
     customer.setCompany({
