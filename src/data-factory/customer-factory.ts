e1fc8c6920c721eedabdd4023cf561c7def43ce7
Vinh Le <VinhleAvantage@gmail.com>
2026-02-08 21:45:01 +0700

    Update test customer-factor

--- src/data-factory/customer-factory.ts
+++ src/data-factory/customer-factory.ts
@@ -33,51 +33,8 @@ export class CustomerFactory {
     const ssoProvider = overrides?.ssoProvider ?? null;
     const ssoToken = overrides?.ssoToken ?? null;
 
-<<<<<<< .mine
     //Admin Portal specific info
     const userType = overrides?.userType ?? "0"; // Default to '0' - Admin;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
-        const customer = new MembPortalCustomer();
-
-        const ts = format(new Date(), 'yyyyMMddHHmmss');
-        const seq = DataGenerate.getRandomInt(1, 9999);
-        const firstName = overrides?.firstName ?? await DataGenerate.generateFirstName();
-        const localPrefix = overrides?.firstName ?? `${firstName}${seq}`;
-        const email = overrides?.email ?? `${localPrefix}@yopmail.com`;
-        const password = overrides?.password ?? `Vl@${ts.slice(-8)}`;
-        const lastName = overrides?.lastName ?? await DataGenerate.generateLastName();
-        const jobTitle = overrides?.jobTitle ?? await DataGenerate.generatejobTitle();
-        const companyName = overrides?.companyName ?? await DataGenerate.generateCompanyName();
-        const companySize = overrides?.companySize ?? '';
-        const phoneNumber = overrides?.phoneNumber ?? await DataGenerate.generatePhoneNumber();
-        const partnerId = overrides?.partnerId?? null;
-        const source = overrides?.source ?? 'member';
-        const departmentId = overrides?.departmentId ?? '688897d5eb52b4af5573def4';
-        const ssoProvider = overrides?.ssoProvider ?? null;
-        const ssoToken = overrides?.ssoToken ?? null;
-
-        //Admin Portal specific info
-    const userType = overrides?.userType ?? '0' // Default to '0' - Admin;
->>>>>>> .theirs
     const useCredit = overrides?.useCredit ?? false;
 
     const statesEmployee = overrides?.statesEmployee ?? [];
