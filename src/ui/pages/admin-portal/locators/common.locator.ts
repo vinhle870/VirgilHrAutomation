a2bc529b5656d09e2bba54503846a94479ebecbb
Vinh Le <VinhleAvantage@gmail.com>
2026-05-05 10:54:43 +0700

    Update common.ts in admin portal

---
+++ src/ui/pages/admin-portal/locators/common.locator.ts
@@ -0,0 +1,8 @@
+export class CommonLocator {
+  public static readonly managementCategory =
+    "xpath=//span[text()='Management']";
+  public static readonly partnerManagement =
+    "xpath=//span[text()='Partner Management']";
+  public static readonly customerManagement =
+    "xpath=//span[text()='Customer Management' and @class='fg1']";
+}
